a435b0419b19bf29c64b1abae4b1f41180d76896
Jobanpreet Singh <jobanpreet1532@gmail.com>
2025-10-01 16:44:13 +0530

    added exports

---
+++ addExport.js
@@ -0,0 +1,7 @@
+// for exporting only a specific piece of code
+
+const add = (a,b) =>{
+    return a + b
+}
+
+module.exports = add
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-require('./add.js')
+// require('./add.js') one way
 // importing the add module using commonJS
 
-console.log("Hello from the index.js")
 
+// const add = require("./addExport")
+
+// console.log("Hello from the index.js")
+
+// const sum = add( 7,7)
+// console.log(sum);
+
+
+// IFFE 
+require("./batman")
+require("./superman")
