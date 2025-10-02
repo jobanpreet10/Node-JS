51dd7e7076bdb895de84c37595031514f689f314
Jobanpreet Singh <jobanpreet1532@gmail.com>
2025-10-02 15:06:52 +0530

    json added

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// require('./add.js') one way
-// importing the add module using commonJS
+const data = require("./data.json")
 
-
-// const add = require("./addExport")
-
-// console.log("Hello from the index.js")
-
-// const sum = add( 7,7)
-// console.log(sum);
-
-
-// IFFE 
-// require("./batman")
-// require("./superman")
-
-// const superhero = require("./super_hero")
-
-// console.log(superhero.getName())
-
-// superhero.setName("superman")
-// console.log(superhero.getName())
-
-// const newSuperhero = require("./super_hero")
-// console.log(newSuperhero.getName())
-
-
-const add = require("./math")
-console.log(add(5,6))
+console.log(data.address)
